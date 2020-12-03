d02ad3df306e9579c6796cbb2fe4d599dcc69909
Hanan Li <hananli@gatech.edu>
2020-12-03 17:38:26 -0500

    trial

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ import psycopg2
 import time
 import random
 import getopt
+import threading
 from statistics import mean
 
 
 
-ip_list = ['24.98.255.22', '52.201.253.38'] #, '54.209.168.101', '54.221.17.161'
+ip_list = ['54.209.168.101'] #,  '24.98.255.22', '52.201.253.38' , '54.209.168.101', '54.221.17.161'
+
 proxy_connection = psycopg2.connect(user = "postgres",
                                     password = "realSmooth",
                                     host = '127.0.0.1',
@@ -27,35 +29,66 @@ def init_conn(ip_list) :
         conn.append(connection)
     return conn
     
+def bulk_insert(queries, idx):
+    time.sleep(1)
+    query = "".join(queries)
+    start_time = time.time()
+    proxy_connection = psycopg2.connect(user = "postgres",
+                                    password = "realSmooth",
+                                    host = '127.0.0.1',
+                                    port = "6432",
+                                    database = "ChaCha")
+    cursor = proxy_connection.cursor()
+    cursor.execute(query)
+    proxy_connection.commit()
+    proxy_connection.close()
+    commit_time = time.time() - start_time
+    print("Thread ", idx, " insert time: ", commit_time)
 
-def insert_query(conn, bulk, num_queries) :
-    
+
+def insert_query(conn, bulk, num_queries, num_writers):
+    global proxy_connection
     query = []
     for i in range(num_queries) :
         query.append(f"INSERT INTO pgbench_history (tid, bid, aid, delta, mtime, filler) VALUES ({random.randrange(1, 10000, 1)}, {random.randrange(1, 1000, 1)}, {i + 1}, {random.randrange(-1000, 1000, 1)}, now(), '');")
 
     start_time = time.time()
+    commit_time = 0.0
     cursor = proxy_connection.cursor()
 
     truncate = "TRUNCATE pgbench_history;"
     cursor.execute(truncate)
-    proxy_connection.commit ()
+    proxy_connection.commit()
+    proxy_connection.close()
 
     if bulk == 0 :
 
         for i in range(num_queries) :
+            start_time = time.time()
+            proxy_connection = psycopg2.connect(user = "postgres",
+                                    password = "realSmooth",
+                                    host = '127.0.0.1',
+                                    port = "6432",
+                                    database = "ChaCha")
+            cursor = proxy_connection.cursor()
             cursor.execute(query[i])
-            proxy_connection.commit ()
-
+            proxy_connection.commit()            
+            proxy_connection.close()
+            commit_time += time.time() - start_time
+        print("IP: proxy ", " insert ", num_queries, " queries time: ", commit_time)
+        cursor.close ()
     else :
-
-        query = "".join(query)
-        cursor.execute(query)
-        proxy_connection.commit ()
-
-    commit_time = time.time() - start_time
-    print("IP: proxy ", " insert ", num_queries, " queries time: ", commit_time)
-    cursor.close ()
+        threads = []
+        for i in range(num_writers):
+            sub_queries = query[i:(i+1) * num_writers/3]
+            t = threading.Thread(bulk_insert, args=(sub_queries, i))
+            threads.append(t)
+        
+        for t in threads:
+            t.join()
+        print("All Threads Done")
+    # commit_time = time.time() - start_time
+    
 
 
 def check_sub(conn, num_queries) :
@@ -63,7 +96,6 @@ def check_sub(conn, num_queries) :
     done_flag = [False for i in range(len(conn))]
     start_time = time.time()
     total_time = [0.0 for i in range(len(conn))]
-
     while True :
         for i in range(len(conn)) :
 
@@ -101,7 +133,7 @@ def main(argv):
         sys.exit(2)
     
     try :
-        opts, args = getopt.getopt(argv, "ub:n:")
+        opts, args = getopt.getopt(argv, "ub:n:w:")
     except getopt.GetoptError: 
         print ("For Usage: run_test.py -u")
         sys.exit(2)
@@ -112,6 +144,7 @@ def main(argv):
             print("-b 1 -> For bulk testing")
             print("-b 0 -> For single testing")
             print("-n <int> -> Number of Inserts to be done")
+            print("-w <int> -> Number of Writers")
             sys.exit(2)
 
         elif opt == '-b' :
@@ -122,6 +155,8 @@ def main(argv):
 
         elif opt == '-n' :
             num_queries = int(arg)
+        elif opt == '-w' :
+            num_writers = int(arg)
 
     if '-b' not in argv or '-n' not in argv :
         print ("For Usage: run_test.py -u")
@@ -129,7 +164,7 @@ def main(argv):
 
 
     conn = init_conn(ip_list)
-    insert_query(conn, bulk, num_queries)
+    insert_query(conn, bulk, num_queries, num_writers)
     
     if check_sub(conn, num_queries) == True :
         for i in range (len(conn)) : conn[i].close()
